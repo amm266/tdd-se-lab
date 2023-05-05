8c8f3258da5ad80dd173391b2e9e2e6679bea9c2
amm <a.m.mohammadi266@gmail.com>
2023-05-05 16:46:05 +0430

    add Square class

---
+++ src/main/java/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    private float length;
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float calculateArea(){
+        return length * length;
+    }
+}
